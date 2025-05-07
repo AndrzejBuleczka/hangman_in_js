e4e2247266609650308ae26435dabdde41377cd7
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-05-07 16:12:52 +0200

    Add Quote class and refactor Game class to utilize it

--- app/game.js
+++ app/game.js
@@ -1,28 +1,34 @@
-class Game {
+import Quote from "./quote.js";
 
+class Game {
   quotes = [
     {
       text: "pan tadeusz",
-      category: "Utwór literacki",
+      category: "Utwór literacki"
     },
     {
       text: "janko muzykant",
-      category: "Utwór literacki",
+      category: "Utwór literacki"
     },
     {
       text: "akademia pana kleksa",
-      category: "Film",
+      category: "Film"
     },
     {
       text: "ogniem i mieczem",
-      category: "Film",
-    },
+      category: "Film"
+    }
   ];
   constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
     this.lettersWrapper = lettersWrapper;
     this.categoryWrapper = categoryWrapper;
     this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
+
+    const { text, category } =
+      this.quotes[Math.floor(Math.random() * this.quotes.length)];
+    this.categoryWrapper.innerHTML = category;
+    this.quote = new Quote(text);
   }
 
   guess(letter) {
@@ -30,13 +36,19 @@ class Game {
     // Here you would implement the logic to check if the guessed letter is in the word
     // and update the game state accordingly.
   }
-  start() {
+
+  drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
-      button.addEventListener('click', () => {this.guess(label)})
+      button.addEventListener("click", () => {
+        this.guess(label);
+      });
       this.lettersWrapper.appendChild(button);
     }
+  }
+  start() {
+    this.drawLetters();
   }
 }
