e7ccc9bc4766b4e708abc89030a2515c4b6ba017
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-05-07 15:56:24 +0200

    Refactor game logic and update HTML structure for Hangman game

--- app/game.js
+++ app/game.js
@@ -1,8 +1,42 @@
 class Game {
+
+  quotes = [
+    {
+      text: "pan tadeusz",
+      category: "Utwór literacki",
+    },
+    {
+      text: "janko muzykant",
+      category: "Utwór literacki",
+    },
+    {
+      text: "akademia pana kleksa",
+      category: "Film",
+    },
+    {
+      text: "ogniem i mieczem",
+      category: "Film",
+    },
+  ];
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+  }
+
+  guess(letter) {
+    console.log("Guessing letter:", letter);
+    // Here you would implement the logic to check if the guessed letter is in the word
+    // and update the game state accordingly.
+  }
   start() {
-    console.log("Game started!");
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = label;
+      button.addEventListener('click', () => {this.guess(label)})
+      this.lettersWrapper.appendChild(button);
+    }
   }
 }
-
-const game = new Game();
-game.start();
